c017dd9bb429a2837b15acb6a52b36474981014d
itai brand <67857476+BrandMan2299@users.noreply.github.com>
2020-08-24 18:58:58 +0300

    materialUi added + start of ticket component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import Ticket from './components/Ticket'
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const [tickets, setTickets] = useState([]);
+
+  useEffect(() => {
+    const onMount = async () => {
+      const { data } = await axios.get("/api/tickets");
+      setTickets(data);
+    }
+    onMount();
+  }, [])
+
   return (
     <main>
+      {tickets.map((ticket, index) => {
+        return <Ticket key={index} ticket={ticket} />
+      })}
     </main>
   );
 }
