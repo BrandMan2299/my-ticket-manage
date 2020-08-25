4bd327d5044387516fea5fb49256c9c8e752abdd
itai brand <67857476+BrandMan2299@users.noreply.github.com>
2020-08-25 12:49:31 +0300

    filtering on search change

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,30 @@
 import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import Ticket from './components/Ticket'
+import NavBar from './components/NavBar';
 import './App.css';
 import axios from 'axios';
 
 function App() {
 
   const [tickets, setTickets] = useState([]);
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
-    const onMount = async () => {
-      const { data } = await axios.get("/api/tickets");
+    const onRender = async () => {
+      const { data } = await axios.get(`/api/tickets?searchText=${searchText}`);
       setTickets(data);
     }
-    onMount();
-  }, [])
+    onRender();
+  }, [searchText])
+
+  const onChange = (e) => {
+    setSearchText(e.target.value);
+  }
 
   return (
     <main>
+      <NavBar onChange={onChange} />
       {tickets.map((ticket, index) => {
         return <Ticket key={index} ticket={ticket} />
       })}
--- client/src/components/Ticket.js
+++ client/src/components/Ticket.js
@@ -1,25 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 
-const useStyles = makeStyles({
-    root: {
-        minWidth: 275,
-    },
-    title: {
-        fontSize: 14,
-    },
-    pos: {
-        marginBottom: 12,
-    },
-});
 
 export default function Ticket(props) {
-    const classes = useStyles();
 
     return (
         <Card className="ticket" elevation={20} variant="outlined">
@@ -33,14 +20,15 @@ export default function Ticket(props) {
                 <Typography className="content" color="textSecondary">
                     {props.ticket.content}
                 </Typography>
-                {/* <Typography variant="body2" component="p">
-                    well meaning and kindly.
-                    <br />
-                    {'"a benevolent smile"'}
-                </Typography> */}
-                <CardActions style={{ display: "flex", flexDirection: "row-reverse" }}>
-                    <Button size="small">Hide</Button>
-                </CardActions>
+                <Typography variant="body2" component="p">
+                    {props.ticket.labels && props.ticket.labels.map((label, index) => {
+                        return (
+                            <button className="label" key={index}>
+                                {label}
+                            </button>
+                        )
+                    })}
+                </Typography>
             </CardContent>
         </Card>
     );
